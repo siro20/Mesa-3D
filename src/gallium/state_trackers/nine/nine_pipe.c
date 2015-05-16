3453ea76a33b3258dc69ee7603ed787257916267
Axel Davy <axel.davy@ens.fr>
2015-05-16 18:41:51 +0200

    st/nine: Always set point_quad_rasterization to 1
    Both Points and Point Sprites are rasterized like quads,
    according to d3d9 doc and gallium rasterizer doc.
    
    Signed-off-by: Axel Davy <axel.davy@ens.fr>

--- src/gallium/state_trackers/nine/nine_pipe.c
+++ src/gallium/state_trackers/nine/nine_pipe.c
@@ -93,7 +93,7 @@ nine_convert_rasterizer_state(struct pipe_rasterizer_state *rast_state, const DW
  /* rast.poly_stipple_enable = 0; */
  /* rast.point_smooth = 0; */
     rast.sprite_coord_mode = PIPE_SPRITE_COORD_UPPER_LEFT;
-    rast.point_quad_rasterization = !!rs[D3DRS_POINTSPRITEENABLE];
+    rast.point_quad_rasterization = 1;
     rast.point_size_per_vertex = rs[NINED3DRS_VSPOINTSIZE];
     rast.multisample = !!rs[D3DRS_MULTISAMPLEANTIALIAS];
     rast.line_smooth = !!rs[D3DRS_ANTIALIASEDLINEENABLE];
